2f62928dc1d9f6cd9e9b941d76b5f5b7f4d7ce73
katrina-o <ekaterinaorlova25041994@gmail.com>
2025-03-04 11:08:42 +0300

    fix: fix fonts

--- Shoppe/Utilites/Fonts.swift
+++ Shoppe/Utilites/Fonts.swift
@@ -9,11 +9,10 @@ import UIKit
 
 enum Fonts {
     static let baseFont: UIFont = UIFont.systemFont(ofSize: 16) // заменить
-    static let nunitoBold: UIFont = UIFont(name: "NunitoSans-Bold", size: 52) ?? UIFont.systemFont(ofSize: 52)
     static let nunitoLight: UIFont = UIFont(name: "NunitoSans-Light", size: 19) ?? UIFont.systemFont(ofSize: 19)
     static let nunitoRegular: UIFont = UIFont(name: "NunitoSans-Regular", size: 12) ?? UIFont.systemFont(ofSize: 12)
     static let nunitoSemiBold: UIFont = UIFont(name: "NunitoSans-SemiBold", size: 13) ?? UIFont.systemFont(ofSize: 13)
-    static let ralewayBoldStartScreenFont: UIFont = UIFont(name: "Raleway-Bold", size: 52) ?? UIFont.systemFont(ofSize: 52)
+    static let ralewayBold52: UIFont = UIFont(name: "Raleway-Bold", size: 52) ?? UIFont.systemFont(ofSize: 52)
     static let ralewayBold: UIFont = UIFont(name: "Raleway-Bold", size: 28) ?? UIFont.systemFont(ofSize: 28)
     static let ralewayExtraBold: UIFont = UIFont(name: "Raleway-ExtraBold", size: 20) ?? UIFont.systemFont(ofSize: 20)
     static let ralewayMedium: UIFont = UIFont(name: "Raleway-Medium", size: 18) ?? UIFont.systemFont(ofSize: 18)
