3079b7e079cfe954c03ae96ac5ef764372d7ce7e
lord-of-the-lost <ignatov.99@yandex.ru>
2025-03-09 09:56:20 +0300

    Add ChipsView

---
+++ Shoppe/CommonUI/ChipsView.swift
@@ -0,0 +1,61 @@
+//
+//  ChipsView.swift
+//  Shoppe
+//
+//  Created by Николай Игнатов on 08.03.2025.
+//
+
+import UIKit
+
+struct ChipsViewModel {
+    let text: String
+}
+
+final class ChipsView: UIView {
+    private lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .center
+        label.font = Fonts.ralewayRegular.withSize(14)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    init(cornerRadius: CGFloat) {
+        super.init(frame: .zero)
+        setupView()
+        setupConstraints()
+        layer.cornerRadius = cornerRadius
+    }
+    
+    @available(*, unavailable, message: "unavailable")
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+// MARK: - ConfigurableViewProtocol
+extension ChipsView: ConfigurableViewProtocol {
+    func configure(with model: ChipsViewModel) {
+        titleLabel.text = model.text
+    }
+}
+
+// MARK: - Private Methods
+private extension ChipsView {
+    func setupView() {
+        addSubview(titleLabel)
+        backgroundColor = .customGray
+    }
+    
+    func setupConstraints() {
+        NSLayoutConstraint.activate([
+            titleLabel.topAnchor.constraint(equalTo: topAnchor, constant: 5),
+            titleLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 12),
+            titleLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -12),
+            titleLabel.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -5)
+        ])
+    }
+}
+
+
+
