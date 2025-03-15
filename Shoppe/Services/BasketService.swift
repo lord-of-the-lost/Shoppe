cea2de1d426224a1767f6d9d45f0aa0a558454fc
lord-of-the-lost <ignatov.99@yandex.ru>
2025-03-15 16:42:42 +0300

    Fix basketService

--- Shoppe/Services/BasketService.swift
+++ Shoppe/Services/BasketService.swift
@@ -10,10 +10,11 @@ import Foundation
 // MARK: - Basket Service
 protocol BasketServiceProtocol {
     var items: [Product] { get }
-    var itemsCount: Int { get }
+    var totalItemsCount: Int { get }
     func addItem(_ product: Product)
     func removeItem(_ product: Product)
     func contains(_ product: Product) -> Bool
+    func updateQuantity(for productId: Int, newQuantity: Int)
     func clearBasket()
 }
 
@@ -28,19 +29,24 @@ final class BasketService: BasketServiceProtocol {
         return user.cart
     }
     
-    var itemsCount: Int { items.count }
+    var totalItemsCount: Int {
+        items.reduce(0) { $0 + $1.count }
+    }
     
     func addItem(_ product: Product) {
-        guard
-            var user: User = userDefaultsService.getCustomObject(forKey: .userModel),
-            !contains(product)
-        else { return }
+        guard var user: User = userDefaultsService.getCustomObject(forKey: .userModel) else { return }
         
-        var updatedProduct = product
-        updatedProduct.isInCart = true
-        user.cart.append(updatedProduct)
+        if let index = user.cart.firstIndex(where: { $0.id == product.id }) {
+            user.cart[index].count += 1
+        } else {
+            var updatedProduct = product
+            updatedProduct.isInCart = true
+            updatedProduct.count = 1
+            user.cart.append(updatedProduct)
+        }
+        
         userDefaultsService.saveCustomObject(user, forKey: .userModel)
-        NotificationCenter.default.post(name: .basketDidUpdate, object: nil)
+        postUpdate()
     }
     
     func removeItem(_ product: Product) {
@@ -48,11 +54,27 @@ final class BasketService: BasketServiceProtocol {
         
         user.cart.removeAll { $0.id == product.id }
         userDefaultsService.saveCustomObject(user, forKey: .userModel)
-        NotificationCenter.default.post(name: .basketDidUpdate, object: nil)
+        postUpdate()
     }
     
     func contains(_ product: Product) -> Bool {
         items.contains { $0.id == product.id }
+    }
+    
+    func updateQuantity(for productId: Int, newQuantity: Int) {
+        guard
+            var user: User = userDefaultsService.getCustomObject(forKey: .userModel),
+            let index = user.cart.firstIndex(where: { $0.id == productId })
+        else { return }
+        
+        if newQuantity <= 0 {
+            user.cart.remove(at: index)
+        } else {
+            user.cart[index].count = newQuantity
+        }
+        
+        userDefaultsService.saveCustomObject(user, forKey: .userModel)
+        postUpdate()
     }
     
     func clearBasket() {
@@ -60,6 +82,10 @@ final class BasketService: BasketServiceProtocol {
         
         user.cart.removeAll()
         userDefaultsService.saveCustomObject(user, forKey: .userModel)
+        postUpdate()
+    }
+    
+    private func postUpdate() {
         NotificationCenter.default.post(name: .basketDidUpdate, object: nil)
     }
 }
